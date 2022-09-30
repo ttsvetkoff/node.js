f39ee7c4fb24969c7b5dba31c82bb0d40be511d8
TT <49660924+ttsvetkoff@users.noreply.github.com>
2022-09-30 12:46:15 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 ssh ubuntu@13.42.51.83 <<EOF
-cp Dockerfile /opt/Dockerfile
-sudo docker build -t ttsvetkoff/node ./ -f /opt/Dockerfile
+sudo docker build -t ttsvetkoff/node ./ -f ~/opt/Dockerfile
 EOF
